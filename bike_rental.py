0a8a69c4b29410f33f8bdad6e5dffdc6377f973d
KubaloPL <superkubalo123@gmail.com>
2024-12-01 18:46:21 +0100

    milestone 1

---
+++ bike_rental.py
@@ -0,0 +1,42 @@
+import json
+import datetime
+import smtplib
+import os
+
+def rent_bike(customer_name, rental_duration): #TODO
+    '''
+        Dodaje wynajem roweru
+    '''
+
+def calculate_cost(rental_duration): #TODO
+    '''
+        Oblicza koszt wynajmu
+    '''
+
+def save_rental(rental): #TODO
+    '''
+        Zapisuje dane wynajmu do pliku JSON.
+    '''
+
+def load_rentals(): #TODO
+    '''
+        Wczytuje wynajmy z pliku JSON.
+    '''
+
+def cancel_rental(customer_name): #TODO
+    '''
+        Usuwa wynajem.
+    '''
+    
+def send_rental_invoice_email(customer_email, rental_details): #TODO
+    '''
+        Wysyła e-mail z fakturą.
+    '''
+
+
+def sgenerate_daily_report(): #TODO
+    '''
+        Generuje raport dzienny.
+    '''
+
+
