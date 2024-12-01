35b7a18ab07b3bd98979aba154bdf96604261fd1
KubaloPL <superkubalo123@gmail.com>
2024-12-01 19:36:51 +0100

    milestone 2

--- bike_rental.py
+++ bike_rental.py
@@ -3,20 +3,39 @@ import datetime
 import smtplib
 import os
 
-def rent_bike(customer_name, rental_duration): #TODO
+rental = {}
+
+RENTALS_PATH = "data/rentals.json"
+
+def rent_bike(customer_name, rental_duration):
     '''
         Dodaje wynajem roweru
     '''
+    cost = calculate_cost(rental_duration)
+    rental[customer_name] = {"rental_duration": rental_duration, "cost": cost}
 
-def calculate_cost(rental_duration): #TODO
+    save_rental(rental)
+
+def calculate_cost(rental_duration: int) -> int:
     '''
         Oblicza koszt wynajmu
-    '''
+        Calculates the cost of a rental from duration in hours
 
-def save_rental(rental): #TODO
+        returns cost of rental in zł
+    '''
+    cost = 10 + (rental_duration - 1) * 5
+    return cost
+
+def save_rental(rental):
     '''
         Zapisuje dane wynajmu do pliku JSON.
     '''
+    if not os.path.exists(RENTALS_PATH):
+        open(RENTALS_PATH,"x")
+
+    file = open(RENTALS_PATH,"w")
+    json.dump(rental, file)
+    file.close()
 
 def load_rentals(): #TODO
     '''
@@ -40,3 +59,6 @@ def sgenerate_daily_report(): #TODO
     '''
 
 
+rent_bike("test", 10)
+
+print(rental)
